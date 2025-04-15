984efa72e48bfc1824d4e071386b43c9de68e12e
KIM Hyunjin <snowrabbit123@naver.com>
2025-04-15 22:20:58 +0900

    6749

---
+++ 6749.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 김현진 on 2025. 4. 15..
+//
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int num1, num2;
+  cin >> num1 >> num2;
+  int differ = num2 - num1;
+  cout << num2 + differ << endl;
+  return 0;
+
+}
